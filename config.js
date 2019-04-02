0d039fe50d67c6c18d88f764e4f9f46da2418bf3
manjunath.pv@psiog.com <manjunath.pv@psiog.com>
2019-04-02 17:04:35 +0530

    Mysql Connection

--- config.js
+++ config.js
@@ -1,16 +1,12 @@
 'use strict'
 
-const ldapServerDomain = process.env.LDAP_SERVER_DOMAIN
-const ldapServerPort = process.env.LDAP_SERVER_PORT
-const ldapAdminUsername = process.env.LDAP_ADMIN_USERNAME
-const ldapAdminPassword = process.env.LDAP_ADMIN_PASSWORD
-const nodeRedBaseUrl = process.env.NODE_RED_BASE_URL
-const discoveryEnvId = process.env.DISCOVERY_ENV_ID
-const discoveryCollectionId = process.env.DISCOVERY_COLLECTION_ID
+const mysqlHost = process.env.MYSQL_HOST;
+const mysqlUser = process.env.MYSQL_USER;
+const mysqlPass = process.env.MYSQL_PASS
 
-const preferences = {
-    tempClientId: 1
-}
+// const preferences = {
+//     tempClientId: 1
+// }
 
 const cookieExpiration = 300000
 const sessionSecret = process.env.SESSION_SECRET
@@ -19,12 +15,7 @@ const sessionSecret = process.env.SESSION_SECRET
 module.exports = {
     cookieExpiration: cookieExpiration,
     sessionSecret: sessionSecret,
-    ldapServerDomain: ldapServerDomain,
-    ldapServerPort: ldapServerPort,
-    ldapAdminUsername: ldapAdminUsername,
-    ldapAdminPassword: ldapAdminPassword,
-    nodeRedBaseUrl: nodeRedBaseUrl,
-    discoveryEnvId: discoveryEnvId,
-    discoveryCollectionId: discoveryCollectionId,
-    preferences: preferences
+    mysqlHost: mysqlHost,
+    mysqlUser: mysqlUser,
+    mysqlPass: mysqlPass
 }
--- helpers/db.js
+++ helpers/db.js
@@ -1,109 +1,123 @@
 'use strict';
 
-const Cloudant = require('cloudant');
-const _ = require('lodash');
+const mysql = require('mysql')
+const config = require('../config')
 
-const cloudantUrl = process.env.IBM_CLOUDANT_URL;
+var con = mysql.createConnection({
+    host: config.mysqlHost,
+    user:config.mysqlUser,
+    password:config.mysqlPass
+});
 
-const cloudant = Cloudant({ url: cloudantUrl });
+con.connect(function(err)
+{
+    if(err) throw err;
+    console.log("Connected to the database!!!");
+})
+// const Cloudant = require('cloudant');
+// const _ = require('lodash');
 
-const databases = {
-    conversations: 'conversations',
-    skitter: 'skitter'
-};
+// const cloudantUrl = process.env.IBM_CLOUDANT_URL;
 
-const getAll = (database, callback) => {
-    const db = cloudant.db.use(database);
-    db.list({ include_docs: true }, (error, data) => {
-        if (error) {
-            console.log('Error: Unable to get documant with ', error);
-            return callback(error, null);
-        }
-        return callback(null, { data: formatDbData(data) });
-    });
-};
+// const cloudant = Cloudant({ url: cloudantUrl });
 
-const get = (database, params, callback) => {
-    const db = cloudant.db.use(database);
-    getAll(database, (error, data) => {
-        if (error) {
-            console.log('Error: Unable to get documant with ', error);
-            return callback(error, null);
-        }
-        const filteredData = _.filter(data.data, params);
-        return callback(null, { data: filteredData });
-    });
-};
+// const databases = {
+//     conversations: 'conversations',
+//     skitter: 'skitter'
+// };
 
-const save = (database, params, callback) => {
-    const db = cloudant.db.use(database);
-    db.insert(params, (error, status) => {
-        if (error) {
-            console.log('Error: Unable to insert documant with ', error);
-            return callback(error, null);
-        }
-        callback(null, { status: status });
-    })
-}
+// const getAll = (database, callback) => {
+//     const db = cloudant.db.use(database);
+//     db.list({ include_docs: true }, (error, data) => {
+//         if (error) {
+//             console.log('Error: Unable to get documant with ', error);
+//             return callback(error, null);
+//         }
+//         return callback(null, { data: formatDbData(data) });
+//     });
+// };
 
-const remove = (database, params, callback) => {
-    const db = cloudant.db.use(database);
-    db.destroy(params._id, params._rev, (error, status) => {
-        if (error) {
-            console.log('Error: Unable to remove documant with ', error);
-            return callback(error, null);
-        }
-        return callback(null, { status: status });
-    })
-};
+// const get = (database, params, callback) => {
+//     const db = cloudant.db.use(database);
+//     getAll(database, (error, data) => {
+//         if (error) {
+//             console.log('Error: Unable to get documant with ', error);
+//             return callback(error, null);
+//         }
+//         const filteredData = _.filter(data.data, params);
+//         return callback(null, { data: filteredData });
+//     });
+// };
 
-const indexes = (callback) => {
-    db.index((error, result) => {
-        if (error) {
-            return callback(error, null);
-        }
+// const save = (database, params, callback) => {
+//     const db = cloudant.db.use(database);
+//     db.insert(params, (error, status) => {
+//         if (error) {
+//             console.log('Error: Unable to insert documant with ', error);
+//             return callback(error, null);
+//         }
+//         callback(null, { status: status });
+//     })
+// }
 
-        console.log('The database has %d indexes', result.indexes.length);
-        for (var i = 0; i < result.indexes.length; i++) {
-            console.log('  %s (%s): %j', result.indexes[i].name, result.indexes[i].type, result.indexes[i].def);
-        }
-        return callback(null, { data: result.indexes });
-    })
-}
+// const remove = (database, params, callback) => {
+//     const db = cloudant.db.use(database);
+//     db.destroy(params._id, params._rev, (error, status) => {
+//         if (error) {
+//             console.log('Error: Unable to remove documant with ', error);
+//             return callback(error, null);
+//         }
+//         return callback(null, { status: status });
+//     })
+// };
 
-const addIndex = (indexes, callback) => {
-    db.index(_composeIndexes(indexes), function (er, response) {
-        if (er) {
-            throw er;
-        }
-        console.log('Index creation result: %s', response.result);
-    });
-}
+// const indexes = (callback) => {
+//     db.index((error, result) => {
+//         if (error) {
+//             return callback(error, null);
+//         }
 
-const _composeIndexes = (params) => {
-    if (!params.name || !params.type || !params.fields) {
-        return params
-    }
-    if (params.fields && !Array.isArray(params.fields)) {
-        params.fields = [params.fields]
-    }
-    return { name: params.name, type: params.type, index: { fields: params.fields } }
-}
+//         console.log('The database has %d indexes', result.indexes.length);
+//         for (var i = 0; i < result.indexes.length; i++) {
+//             console.log('  %s (%s): %j', result.indexes[i].name, result.indexes[i].type, result.indexes[i].def);
+//         }
+//         return callback(null, { data: result.indexes });
+//     })
+// }
 
-const formatDbData = (data) => {
-    data = data.rows
-    const dataLength = data.length
-    for (let i = 0; i < dataLength; i++){
-        data[i] = data[i].doc
-    }
-    return data
-}
+// const addIndex = (indexes, callback) => {
+//     db.index(_composeIndexes(indexes), function (er, response) {
+//         if (er) {
+//             throw er;
+//         }
+//         console.log('Index creation result: %s', response.result);
+//     });
+// }
 
-module.exports = {
-    databases: databases,
-    indexes: indexes,
-    get: get,
-    getAll: getAll,
-    save: save,
-    remove: remove
-};
+// const _composeIndexes = (params) => {
+//     if (!params.name || !params.type || !params.fields) {
+//         return params
+//     }
+//     if (params.fields && !Array.isArray(params.fields)) {
+//         params.fields = [params.fields]
+//     }
+//     return { name: params.name, type: params.type, index: { fields: params.fields } }
+// }
+
+// const formatDbData = (data) => {
+//     data = data.rows
+//     const dataLength = data.length
+//     for (let i = 0; i < dataLength; i++){
+//         data[i] = data[i].doc
+//     }
+//     return data
+// }
+
+// module.exports = {
+//     databases: databases,
+//     indexes: indexes,
+//     get: get,
+//     getAll: getAll,
+//     save: save,
+//     remove: remove
+// };
