41e8951653117f2f9e9490630c03bfd8bbe52fcc
manjunath PV <manjunath.pv@psiog.com>
2019-04-08 18:12:50 +0530

    Added a blank route for a start

--- routes/start.js
+++ routes/start.js
@@ -4,9 +4,14 @@ import express from 'express'
 import cogniUtil from './../helpers/cogni-util'
 
 const logger = cogniUtil.logger
+const responseBuilder = cogniUtil.responseBuilder
 
 const app = express()
 
 app.get('/', (req, res) => {
   logger.info('Welcome to Mears!!')
+  const welcomeMsg = 'Welcome to Mears!!'
+  res.json({ 'message': welcomeMsg })
 })
+
+module.exports = app
