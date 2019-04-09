3cbc48859c77ebc34e36bfd4af7f7c4fa84ae331
manjunath PV <manjunath.pv@psiog.com>
2019-04-09 12:21:22 +0530

    implemented registration and login for sqlite3

--- helpers/litedb.js
+++ helpers/litedb.js
@@ -2,33 +2,75 @@ import cogniUtil from './../helpers/cogni-util'
 
 var sqlite3 = require('sqlite3').verbose()
 const logger = cogniUtil.logger
-let db = new sqlite3.Database('../scripts/mears_poc.db', (err) => {
+let db = new sqlite3.Database('./scripts/mears_poc.db', (err) => {
   if (err) {
     console.error(err.message)
+  } else {
+    console.log('Connected to the Mears POC database.')
   }
-  console.log('Connected to the Mears POC database.')
 })
 
-db.serialize(function () {
-  var stmt = db.prepare('INSERT INTO Users VALUES (?,?)')
-  for (var i = 0; i < 10; i++) {
-    stmt.run('Ipsum of ' + i, i)
-  }
-  stmt.finalize()
+// db.serialize(function () {
+//   var stmt = db.prepare('INSERT INTO Users(firstname,lastname) VALUES (?,?)')
+//   for (var i = 0; i < 10; i++) {
+//     stmt.run('Ipsum of ' + i, i)
+//   }
+//   stmt.finalize()
 
-  db.each('SELECT rowid AS id, info FROM lorem', function (err, row) {
-    if (err) {
-      console.log(err)
-    }
-    console.log(row.id + ': ' + row.info)
+//   db.each('SELECT rowid AS id, info FROM lorem', function (err, row) {
+//     if (err) {
+//       console.log(err)
+//     }
+//     console.log(row.id + ': ' + row.info)
+//   })
+// })
+
+const register = (req, res, error) => {
+  if (error) {
+    logger.info(error)
+  }
+
+  var firstname = req.body.firstname
+  var lastname = req.body.lastname
+  var username = req.body.username
+  var password = req.body.password
+
+  db.serialize(function () {
+    var stmt = db.prepare('INSERT INTO Users(firstname,lastname,username,password) VALUES (?,?,?,?)')
+    stmt.run(firstname, lastname, username, password)
+    stmt.finalize()
   })
-})
 
-db.close()
-
-const postNames = (req,res,error) =>
-{
-
+  res.json({
+    'message': 'Registered User successfully'
+  })
 }
 
-module.exports = postNames
+const login = (req, res, error) => {
+  if (error) {
+    logger.info(error)
+  }
+
+  var username = req.body.username
+  var password = req.body.password
+
+  db.serialize(function () {
+    db.get(`SELECT id as ID from Users where username = "${username}" AND password = "${password}"`, function (err, row) {
+      if (err) {
+        console.log(err)
+      }
+      return row
+        ? res.json({
+          'message': 'User logged in'
+        })
+        : res.json({ 'error': `Username or password is incorrect` })
+    })
+  })
+}
+
+module.exports = {
+  register: register,
+  login: login
+}
+
+// db.close()
--- routes/auth.js
+++ routes/auth.js
@@ -4,7 +4,8 @@ import _ from 'lodash'
 import express from 'express'
 
 import cogniUtil from './../helpers/cogni-util'
-import auth from './../handlers/auth'
+// import auth from './../handlers/auth
+const liteDB = require('../helpers/litedb')
 
 const logger = cogniUtil.logger
 const errors = cogniUtil.errorHandler
@@ -12,28 +13,24 @@ const responseBuilder = cogniUtil.responseBuilder
 const app = express()
 app.use(express.json())
 
-app.post('/login', (req, res, next) => {
-  if (!_.has(req.body, 'username') || !_.has(req.body, 'password')) {
-    logger.error('Error: Missing or invalid parameters.')
-    return responseBuilder(req, res, errors.missingParameter(true), null)
-  }
-  auth.passport.authenticate('login', (error, sessionDetail) => {
-    console.log(sessionDetail)
-    if (error) {
-      logger.error('Error: Unable to authenticate user with error: ', error)
-      return responseBuilder(req, res, errors.formatErrorForWire(error), null)
-    }
-    responseBuilder(req, res, null, sessionDetail)
-  })(req, res, next)
+app.post('/register', (req, res, next) => {
+  // if (!_.has(req.body, 'username') || !_.has(req.body, 'password')) {
+  //   logger.error('Error: Missing or invalid parameters.')
+  //   return responseBuilder(req, res, errors.missingParameter(true), null)
+  // }
+  // auth.passport.authenticate('login', (error, sessionDetail) => {
+  //   console.log(sessionDetail)
+  //   if (error) {
+  //     logger.error('Error: Unable to authenticate user with error: ', error)
+  //     return responseBuilder(req, res, errors.formatErrorForWire(error), null)
+  //   }
+  //   responseBuilder(req, res, null, sessionDetail)
+  // })(req, res, next)
+  liteDB.register(req, res)
 })
 
-app.post('/logout', (req, res) => {
-  if (!req.header('session_id')) {
-    logger.error('Error: Missing or invalid parameters.')
-    return responseBuilder(req, res, errors.missingParameter(true), null)
-  }
-  req.logout()
-  responseBuilder(req, res, null, null)
+app.post('/login', (req, res) => {
+  liteDB.login(req, res)
 })
 
 module.exports = app
